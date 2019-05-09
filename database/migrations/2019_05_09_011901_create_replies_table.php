b891d65feab466208fb7cb930d73de6c410f8478
Darren Whyte <darrenjevon@gmail.com>
2019-05-08 21:40:21 -0400

    Creating Migration, Model, Controller, Factories

---
+++ database/migrations/2019_05_09_011901_create_replies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('body');
+
+            $table->unsignedInteger('question_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('questions_id')->references('id')->on('questions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
