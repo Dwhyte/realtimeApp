2bf19b222f2b747ffcec56a693955eec3b7270a0
Darren Whyte <darrenjevon@gmail.com>
2019-05-11 20:30:05 -0400

    Vuetify, Vue Router, and Initial Components

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,12 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import router from './Router/router'
+
+
+Vue.use(Vuetify)
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,6 +26,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('app-home', require('./components/AppHome.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +36,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <toolbar></toolbar>
+    <appFooter></appFooter>
+  </div>
+</template>
+
+<script>
+import toolbar from "./Toolbar";
+import AppFooter from "./AppFooter";
+export default {
+  components: {
+    toolbar: toolbar,
+    appFooter: AppFooter
+  }
+};
+</script>
+
+<style>
+</style>
